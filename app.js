b3b0bdb56650f32b6186ec517ed0e7d284272321
Leo <squall.ryudo@gmail.com>
2025-03-14 10:37:19 -0300

    actualizacion de README

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const listaAmigos = document.getElementById('listaAmigos');
 const resultado = document.getElementById('resultado');
 const amigoInput = document.getElementById('amigo');
 const amigos = [];
-const botonSortear = document.querySelector('.button-draw');
+const botonSortear = document.querySelector('.button-draw'); //Variable para deshabilitar el boton sortear
+const botonAgregar = document.querySelector('.button-add'); // Variable para deshabilitar el boton agregar
+let sorteoRealizado = false; // Variable para controlar si se ha realizado el sorteo
 
 function agregarAmigo() {
   const nombre = amigoInput.value.trim();
@@ -20,15 +22,15 @@ function agregarAmigo() {
     const nuevoLi = document.createElement('li');
     nuevoLi.textContent = nombre;
     listaAmigos.appendChild(nuevoLi);
-    amigoInput.value = ""; // Limpiar el input
+    amigoInput.value = ""; // Limpia el texto
   }
   
 }
 
 
 function sortearAmigo() {
-  if (amigos.length === 0) {
-    alert("Debes ingresar al menos un amigo para el sorteo.");
+  if (amigos.length < 2) { //verifica si al menos ahi 2 amigos agregados
+    alert("Debes ingresar al menos dos amigos para el sorteo.");
     return;
   }
 
@@ -44,12 +46,15 @@ function sortearAmigo() {
   resultadoLi.textContent = `El amigo seleccionado es: ${amigoSeleccionado}`;
   resultado.appendChild(resultadoLi);
   listaAmigos.innerHTML = ""; // Limpiar la lista de amigos en pantalla
-  botonSortear.disabled = true;
+  botonSortear.disabled = true; //deshabilita el boton "sortear"
+  botonAgregar.disabled = true; // Deshabilitar el botón "Añadir"
+  sorteoRealizado = true; // Indica que el sorteo se ha realizado
+  
 }
 
 // Evento para agregar con Enter
 amigoInput.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) { // 13 es el código de la tecla Enter
-        agregarAmigo();
-    }
+  if (event.keyCode === 13 && !sorteoRealizado) { // Verifica si Enter y si el sorteo NO se ha realizado
+    agregarAmigo();
+  }
 });
